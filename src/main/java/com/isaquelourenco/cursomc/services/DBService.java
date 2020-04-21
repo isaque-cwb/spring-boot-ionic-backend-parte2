9bfec6269ed7dc944119604da5c7f0208c73a9c1
Isaque Lourenco <isaque.lourenco2@gmail.com>
2020-04-20 23:56:56 -0300

    acesso ao email atravez de senha de app gerada

--- src/main/java/com/isaquelourenco/cursomc/services/DBService.java
+++ src/main/java/com/isaquelourenco/cursomc/services/DBService.java
@@ -120,7 +120,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "isaque.lourenco2@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "isaque.modern@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("273363323", "41 99565-5887"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "apto 303", "Jardim", "38220834", cli1, c1);
