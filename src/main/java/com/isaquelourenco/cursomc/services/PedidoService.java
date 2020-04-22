c84436e96441bd636bf751be7d862290ee0c0ba4
Isaque Lourenco <isaque.lourenco2@gmail.com>
2020-04-22 13:35:57 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/isaquelourenco/cursomc/services/PedidoService.java
+++ src/main/java/com/isaquelourenco/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.isaquelourenco.cursomc.domain.Cliente;
 import com.isaquelourenco.cursomc.domain.ItemPedido;
 import com.isaquelourenco.cursomc.domain.PagamentoComBoleto;
 import com.isaquelourenco.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.isaquelourenco.cursomc.enums.EstadoPagamento;
 import com.isaquelourenco.cursomc.repositories.ItemPedidoRepository;
 import com.isaquelourenco.cursomc.repositories.PagamentoRepository;
 import com.isaquelourenco.cursomc.repositories.PedidoRepository;
+import com.isaquelourenco.cursomc.security.UserSS;
+import com.isaquelourenco.cursomc.services.exceptions.AuthorizationException;
 import com.isaquelourenco.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -69,6 +75,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 	
 }
