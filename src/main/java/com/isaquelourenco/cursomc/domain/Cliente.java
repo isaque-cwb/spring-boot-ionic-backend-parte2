f5fb3ca8519daa315434a1fc6d5e56679196c28a
Isaque Lourenco <isaque.lourenco2@gmail.com>
2020-04-23 03:48:35 -0300

    Usando padrão de nomes para imagem

--- src/main/java/com/isaquelourenco/cursomc/domain/Cliente.java
+++ src/main/java/com/isaquelourenco/cursomc/domain/Cliente.java
@@ -52,8 +52,7 @@ public class Cliente  implements Serializable{
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
-	
+		
 	public Cliente () {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -174,17 +173,6 @@ public class Cliente  implements Serializable{
 		return true;
 	}
 
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-
-	
-
-	
-	
+		
 	
 }
